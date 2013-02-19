accc9545c0ef7c8bfa0bc9b73d113b576da832ed
Dantepy <xiangduxiuzy@gmail.com>
2013-02-20 01:49:57 +0800

    bootGene v0.5

--- DI/identify.R
+++ DI/identify.R
@@ -31,5 +31,26 @@ mergeNetwork<-function(net1, net2){
 bootGene<-function(ctrl,tumor,B){
   #ctrl, tumor: gene expression data for ctrl group and tumor group respectively;
   #B: boostrap times;
-  
+  res<-c()
+  b<-0
+  N<-ncol(ctrl) #number of examples
+  indices<-seq(1,N)
+  while(b<B){
+    b_indices<-sample(indices,size=floor(sqrt(N)))
+    b_ctrl_net<-makeNetwork(ctrl[b_indices,],0.75) #OK, we will add parameter later.
+    b_tumor_net<-makeNetwork(tumor[b_indices,],0.75)
+    b_net<-mergeNetwork(b_ctrl_net,b_tumor_net)
+    res<-c(res,b_net)
+    b<-b+1
+  }
+  myCount<-function(x,v){
+    return(sum(v==x))
+  }
+  f_res<-c()
+  for(e in res){
+    if(myCount(e,res)>B/2){
+      f_res<-c(f_res,e)
+    }
+  }
+  return(unique(f_res))
 }
