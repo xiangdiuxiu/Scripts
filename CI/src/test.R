8a4891242a57d91f7d1a91ff80ab49a33ace954a
zhangye <zhangye@node1.(none)>
2013-02-26 02:02:38 +0800

    A bad test Result

--- CI/src/test.R
+++ CI/src/test.R
@@ -2,19 +2,22 @@ source("pca_cmi.R")
 source("pca_dc.R")
 Y50<-read.csv(file="../data/Yeast50",header=T,sep=" ")
 Y50<-Y50[,-1]
-alphas<-seq(0.1,0.9,by=0.1)
+alphas<-seq(0.5,0.9,by=0.1)
 ROCs<-c()
 
-true_net<-read.table(file="../data/DataMatrix.txt",header=F)
+true_net<-read.table(file="../data/true_net",header=F,sep=",")
 true_net<-as.matrix(true_net)
 library(minet)
 for(alpha in alphas){
   dc_net<-pca_dc(t(Y50),alpha,2)
+  dc_net<-dc_net[[1]]
   test_dc.tbl<-validate(dc_net,true_net)
   roc<-auc.roc(test_dc.tbl)
+  show(roc)
   ROCs<-c(ROCs,roc)
 }
-cmi_net<-pca_cmi(t(Y50),0.75,2)
+cmi_net<-pca_cmi(t(Y50),0.75)
+cmi_net<-cmi_net[[1]]
 test_cmi.tbl<-validate(cmi_net,true_net)
 cmiRoc<-auc.roc(test_cmi.tbl)
 show(ROCs)
