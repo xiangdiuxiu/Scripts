e535d6355382106ce5c04bf212d82aee84fefc11
BMI <srtsry@qq.com>
2013-03-02 12:33:12 +0800

    In fact Nothing important changed

--- DC/src/DCtest.R
+++ DC/src/DCtest.R
@@ -15,6 +15,14 @@ mr.tbl<-validate(mr,true)
 clr.tbl<-validate(clr,true)
 dc.tbl<-validate(dc,true)
 
+write.csv(dc,"tbl/dc_dc.csv",row.names=F,col.names=F,quote=F)
+write.csv(mr,"tbl/mr_dc.csv",row.names=F,col.names=F,quote=F)
+write.csv(clr,"tbl/clr_dc.csv",row.names=F,col.names=F,quote=F)
+
 auc.roc(mr.tbl)
 auc.roc(clr.tbl)
 auc.roc(dc.tbl)
+
+auc.pr(mr.tbl)
+auc.pr(clr.tbl)
+auc.pr(dc.tbl)
--- DI/test/new_read_data.cpp
+++ DI/test/new_read_data.cpp
@@ -14,18 +14,7 @@ int main(int argc, char** argv)
   std::istringstream istr;
   string str;
   double tmp;
-  /*
-  getline(fin,str);
-  mat A(str);
-  while(getline(fin,str))
-    {
-      istr.str(str);
-      mat B(str);
-      A = join_cols(A,B);
-      //B.print("B:");
-      //cout<<B.n_elem;
-    }
-  */
+
   mat A = zeros<mat>(17000,160);
   for (int i = 1 ; i < 10000; i++){
     getline(fin,str);
@@ -33,7 +22,9 @@ int main(int argc, char** argv)
     A.row(i) = B;
   }
   cout<<A.n_rows<<endl;
-  mat C = cov(A.t());
-  cout<<C.n_rows<<endl;
+  A = A.t();
+  for (int i = 0 ; i < 100 ; i++)
+    cout<<A.row(i)<<endl;
+  
   return 0;
 }
