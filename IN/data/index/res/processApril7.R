12935597bf13963cdadd43342f410255112da199
Dantepy <xiangduxiuzy@gmail.com>
2013-04-08 01:47:42 +0800

    delay function

--- IN/data/index/res/processApril7.R
+++ IN/data/index/res/processApril7.R
@@ -25,4 +25,37 @@ rel<-rel-diag(ncol(rel))
 #选出与A股指数相关性较高的指数数据
 hr<-which(abs(rel[A,])>0.957)
 nhr<-names(hr)
-typeof(nhr)
+
+##计算“错位时间”下相关性的函数
+delay<-function(data1,data2,window,len){
+	m = length(data1)
+	n = length(data2)
+	if(max(m,n)<(window[2]+len) || min(m,n)<len){
+            show("length error")
+            return(NULL)
+	}
+	if(m==min(m,n)){
+            stData = data1[1:len]
+	    dyData = data2
+	} else {
+	    stData = data2[1:len]
+	    dyData = data1
+	}
+	res = c()
+	for(i in window[1]:window[2]){
+	      tmpData = dyData[(1+i):(len+i)]
+	      tmpres = cor(tmpData,stData)
+	      res = c(res, tmpres)
+	}
+	return(res)
+}
+
+##测试##
+fileA=paste0("clean",A)
+fileT=paste0("clean",nhr[1])
+data=read.table(fileA)
+data1=data$V5
+data=read.table(fileT)
+data2=data$V5
+
+delay(data1,data2,window=c(5,90),len=1000)
